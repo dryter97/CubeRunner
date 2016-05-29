ecc42610f567b4a449512eff64a07a466719bd35
PoutineQc <chagnon.s21@gmail.com>
2016-05-29 09:08:21 -0400

    The Late Update
    To fix the arena not dropping blocks (Decimal formater generating a ','
    instead of a '.')
    and the inventory title size limit.

--- Bukkit/src/me/poutineqc/cuberunner/commands/inventories/CRInventory.java
+++ Bukkit/src/me/poutineqc/cuberunner/commands/inventories/CRInventory.java
@@ -11,39 +11,41 @@ import me.poutineqc.cuberunner.CRPlayer.PlayerStatsException;
 import me.poutineqc.cuberunner.Language.Messages;
 
 public abstract class CRInventory {
-		protected Inventory inventory;
-		protected CRPlayer crPlayer;
-		protected int amountOfRows;
-		protected String title;
+	protected Inventory inventory;
+	protected CRPlayer crPlayer;
+	protected int amountOfRows;
+	protected String title;
 
-		public CRInventory(CRPlayer crPlayer) {
-			this.crPlayer = crPlayer;
-		}
+	public CRInventory(CRPlayer crPlayer) {
+		this.crPlayer = crPlayer;
+	}
 
-		public abstract void fillInventory() throws PlayerStatsException;
+	public abstract void fillInventory() throws PlayerStatsException;
 
-		public abstract void update(ItemStack itemStack, InventoryAction action);
+	public abstract void update(ItemStack itemStack, InventoryAction action);
 
-		public static boolean areEqualOnColorStrip(String itemA, String itemB) {
-			return ChatColor.stripColor(ChatColor.translateAlternateColorCodes('&', itemA))
-					.equalsIgnoreCase(ChatColor.stripColor(ChatColor.translateAlternateColorCodes('&', itemB)));
-		}
+	public static boolean areEqualOnColorStrip(String itemA, String itemB) {
+		return ChatColor.stripColor(ChatColor.translateAlternateColorCodes('&', itemA))
+				.equalsIgnoreCase(ChatColor.stripColor(ChatColor.translateAlternateColorCodes('&', itemB)));
+	}
 
-		protected void createInventory() {
-			inventory = Bukkit.createInventory(crPlayer.getPlayer(), amountOfRows * 9, getFullTitle());
-		}
+	protected void createInventory() {
+		inventory = Bukkit.createInventory(crPlayer.getPlayer(), amountOfRows * 9, getFullTitle());
+	}
 
-		protected String getFullTitle() {
-			return ChatColor.translateAlternateColorCodes('&', crPlayer.getLanguage().get(Messages.PREFIX_LONG) + " " + title);
-		}
-		
-		protected void openInventory() {
-			crPlayer.getPlayer().openInventory(inventory);
-			crPlayer.setCurrentInventory(this);
-		}
-		
-		protected void closeInventory() {
-			crPlayer.getPlayer().closeInventory();
-			crPlayer.setCurrentInventory(null);
-		}
+	protected String getFullTitle() {
+		String title = ChatColor.translateAlternateColorCodes('&',
+				crPlayer.getLanguage().get(Messages.PREFIX_SHORT) + " " + this.title);
+		return title;
+	}
+
+	protected void openInventory() {
+		crPlayer.getPlayer().openInventory(inventory);
+		crPlayer.setCurrentInventory(this);
+	}
+
+	protected void closeInventory() {
+		crPlayer.getPlayer().closeInventory();
+		crPlayer.setCurrentInventory(null);
+	}
 }
